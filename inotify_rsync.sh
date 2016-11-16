f7e0bfcd0d4ffe43bfeb3071df4b7a9456ad5689
L.DANNY <1000359950@smail.shnu.edu.cn>
2016-11-16 11:27:26 +0800

    Update inotify_rsync.sh

--- inotify_rsync.sh
+++ inotify_rsync.sh
@@ -4,5 +4,6 @@ DST=danny@10.249.6.45:/home/danny/test/
 INWT=/usr/bin/inotifywait
 RSYNC=/usr/bin/rsync
 $INWT -mrq -e create,move,delete,modify $SRC | while read D E F ; do
-$RSYNC -aHqz --delete --exclude "\.svn" $SRC $DST #no print log quietly
+#$RSYNC -ravz --delete --exclude=*.log $SRC $DST #print log 
+$RSYNC -aHqz --delete --exclude=*.log $SRC $DST #no print log quietly
 done
